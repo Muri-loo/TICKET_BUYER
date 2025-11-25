39105001601fb74b5f8c4766948a6126536301b3
Murilo Oliveira <murilo.oliveira@criticaltechworks.com>
2025-11-25 14:38:34 +0000

    smallchanges

--- ticket.py
+++ ticket.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
+import os
 
 options = Options()
 # options.add_argument('--headless')
@@ -9,6 +11,6 @@ options = Options()
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://python.org")
-print(driver.title)
+driver.get("https://www.cp.pt/pt/")
+print("Opened: " + driver.title)
 driver.close()
