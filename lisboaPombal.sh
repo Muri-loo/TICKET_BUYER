607f554df105814da9de937f6a83e4dab1957a0c
Murilo Oliveira <murilo.oliveira@ctw.bmwgroup.com>
2026-08-12 16:40:12 +0100

    add git ignore

--- lisboaPombal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POMBAL_STATION=94-34645
-SANTA_APOLONIA_STATION=94-30007
-DEPARTURE_TIME=17:39
-ARRIVAL_TIME=19:07
-
-. /home/murilo-oliveira/CP/TICKET_BUYER/.venv/bin/activate
-python3 /home/murilo-oliveira/CP/TICKET_BUYER/ticket.py $SANTA_APOLONIA_STATION $POMBAL_STATION $DEPARTURE $ARRIVAL > /home/murilo-oliveira/CP/TICKET_BUYER/jobStatus.txt 2>&1
